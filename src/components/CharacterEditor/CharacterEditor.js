4937405283fc7c1ca6bdf19212847c4184ddbba8
David Attlee <pseudogrammer@gmail.com>
2022-09-08 18:26:21 -0400

    Ex 4 - fix order

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -26,7 +26,16 @@ function App() {
   return (
     <main className={styles.characterEditor}>
       <div className={styles.greyBackground} />
-
+      <div className={styles.characterWrapper}>
+        <Character
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+      </div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -74,17 +83,6 @@ function App() {
           />
         </div>
       </MaxWidthWrapper>
-
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
     </main>
   );
 }
